ce795dbcb4fd94b9be4719cb169b57c1abe27d7d
Edmund Dipple <edmund@devopsguys.com>
2016-04-21 16:11:04 +0100

    Test get_terraform_resources

--- terraform_validate/unit_test.py
+++ terraform_validate/unit_test.py
@@ -3,6 +3,16 @@ import terraform_validate
 
 class TestValidatorUnit(unittest.TestCase):
 
+    def test_get_terraform_resource_that_exists(self):
+        resources = {"foo":{u'value':1}}
+        a = terraform_validate.Validator.get_terraform_resources('foo',resources)
+        self.assertEqual(a,{u'value':1})
+
+    def test_get_terraform_resource_that_doesnt_exist(self):
+        resources = {"foo":{u'value':1}}
+        a = terraform_validate.Validator.get_terraform_resources('bar',resources)
+        self.assertEqual(a,None)
+
     def test_resource_property_value_equals_expected_value(self):
         v = terraform_validate.Validator()
         a = v.resource_property_value_equals("foo",{u'value':1},'aws_instance','value',1)
