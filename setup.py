eee60c95766c599ff1ccc197c92095d49f16d30f
Edmund Dipple <edmund@devopsguys.com>
2016-07-13 13:42:57 +0100

    Fix setup.py, update gitignore and create release script

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Edmund Dipple",
     author_email="elmundio1987@gmail.com",
     description="A library that provides asserts for testing Terraform configuration",
-    url="https://github.com/elmundio87/terraform_validator"
+    url="https://github.com/elmundio87/terraform_validator",
     download_url = 'https://github.com/elmundio87/terraform_validator/tarball/1.0',
     keywords = ['terraform', 'assert', 'testing'],
     packages = find_packages(),
