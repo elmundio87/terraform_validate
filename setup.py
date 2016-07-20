e8f1df33841f501cd7505d170fd6508f59839a45
Edmund Dipple <elmundio1987@gmail.com>
2016-07-20 16:16:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="elmundio1987@gmail.com",
     description="A library that provides asserts for testing Terraform configuration",
     url="https://github.com/elmundio87/terraform_validator",
-    download_url = 'https://github.com/elmundio87/terraform_validator/tarball/2.0.1',
+    download_url = 'https://github.com/elmundio87/terraform_validate/tarball/2.0.1',
     keywords = ['terraform', 'assert', 'testing'],
     packages = find_packages(),
     install_requires=[
