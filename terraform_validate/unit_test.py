50de862e8ad41fc53b4cc077d76fa2b0066a9abb
Edmund Dipple <elmundio1987@gmail.com>
2016-12-22 18:58:47 +0000

    Remove deprecated methods (#17)
    * Removed deprecated methods
    
    * Remove unused methods
    
    * Improve test coverage of required_properties methods
    
    * Improve test coverage of disable_variable_expansion
    
    * Added test for multiline regex

--- terraform_validate/unit_test.py
+++ terraform_validate/unit_test.py
@@ -64,6 +64,11 @@ class TestValidatorUnitHelper(unittest.TestCase):
         a = v.matches_regex_pattern('abc_123', '^abc_123$')
         self.assertTrue(a)
 
+    def test_matches_multiline_regex_is_true(self):
+        v = t.Validator()
+        a = v.matches_regex_pattern('abc_\n123', '^abc_.123$')
+        self.assertTrue(a)
+
     def test_matches_regex_is_false(self):
         v = t.Validator()
         a = v.matches_regex_pattern('abc_123', '^abc_321$')
@@ -106,74 +111,6 @@ class TestValidatorUnitHelper(unittest.TestCase):
         self.assertEqual(t.TerraformPropertyList.bool2str(a, False), "False")
         self.assertEqual(t.TerraformPropertyList.bool2str(a, "False"), "False")
 
-    if __name__ == '__main__':
-        suite = unittest.TestLoader().loadTestsFromTestCase(TestValidatorUnitHelper)
-        unittest.TextTestRunner(verbosity=0).run(suite)
-
-class TestValidatorUnitAssertClosures(unittest.TestCase):
-
-    def test_resource_property_value_equals_expected_value(self):
-        v = t.Validator()
-        a = v.resource_property_value_equals("foo",{'value':1},'aws_instance','value',1)
-        self.assertEqual(a,[])
-
-    def test_resource_property_value_equals_unexpected_value(self):
-        v = t.Validator()
-        a = v.resource_property_value_equals("foo",{'value':1},'aws_instance','value',2)
-        self.assertEqual(len(a),1)
-        self.assertEqual(a[0],"[aws_instance.foo.value] should be '2'. Is: '1'")
-
-    def test_resource_property_value_not_equals_expected_value(self):
-        v = t.Validator()
-        a = v.resource_property_value_not_equals("foo", {'value': 1}, 'aws_instance', 'value', 2)
-        self.assertEqual(a, [])
-
-    def test_resource_property_value_not_equals_unexpected_value(self):
-        v = t.Validator()
-        a = v.resource_property_value_not_equals("foo", {'value': 1}, 'aws_instance', 'value', 1)
-        self.assertEqual(len(a), 1)
-        self.assertEqual(a[0], "[aws_instance.foo.value] should not be '1'. Is: '1'")
-
-    def test_resource_has_properties_expected_values(self):
-        v = t.Validator()
-        a = v.resource_has_properties("foo",{'value1':1,'value2':2},'aws_instance',['value1','value2'])
-        self.assertEqual(a,[])
-
-    def test_resource_has_properties_expected_values_one_missing(self):
-        v = t.Validator()
-        a = v.resource_has_properties("foo", {'value1': 1, 'value2': 2}, 'aws_instance', ['value1', 'value2','value3'])
-        self.assertEqual(a, ["[aws_instance.foo] should have property: 'value3'"])
-
-    def test_resource_has_properties_expected_values_two_missing(self):
-        v = t.Validator()
-        a = v.resource_has_properties("foo", {'value1': 1, 'value2': 2}, 'aws_instance', ['value1', 'value2','value3','value4'])
-        self.assertEqual(a, ["[aws_instance.foo] should have property: 'value3'",
-                             "[aws_instance.foo] should have property: 'value4'"])
-
-    def test_resource_value_matches_regex_expected_value(self):
-        v = t.Validator()
-        a = v.resource_property_value_matches_regex("foo", {'value': '1'}, 'aws_instance','value', '[0-9]')
-        self.assertEqual(a, [])
-
-    def test_resource_value_matches_regex_unexpected_value(self):
-        v = t.Validator()
-        a = v.resource_property_value_matches_regex("foo", {'value': 'a'}, 'aws_instance','value', '[0-9]')
-        self.assertEqual(a, ["[aws_instance.foo.value] should match regex '[0-9]'. Is: 'a'"])
-
-    def test_resource_regexproperty_value_equals_expected_value(self):
-        v = t.Validator()
-        a = v.resource_regexproperty_value_equals("foo", {'my_property':1}, 'aws_instance', '^my_.*', 1)
-        self.assertEqual(a, [])
-
-    def test_resource_regexproperty_value_equals_missing_property(self):
-        v = t.Validator()
-        a = v.resource_regexproperty_value_equals("foo", {'mein_property':1}, 'aws_instance', '^my_.*', 1)
-        self.assertEqual(a, ["[aws_instance.foo] No properties were found that match the regex '^my_.*'"])
-
-    def test_resource_regexproperty_value_equals_wrong_value(self):
-        v = t.Validator()
-        a = v.resource_regexproperty_value_equals("foo", {'my_property':2}, 'aws_instance', '^my_.*', 1)
-        self.assertEqual(a, ["[aws_instance.foo.my_property] should be '1'. Is: '2'"])
 
 class TestTerraformVariableParser(unittest.TestCase):
 
@@ -187,8 +124,3 @@ class TestTerraformVariableParser(unittest.TestCase):
         a.parse()
         self.assertEqual(a.variable, 'lol')
         self.assertEqual(a.functions, ['lower','upper'])
-
-
-if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestValidatorUnitAssertClosures)
-    unittest.TextTestRunner(verbosity=0).run(suite)
