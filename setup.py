15f910c059804cf7c80cef2d4f754c434e35d3bb
Edmund Dipple <edmund.dipple@devopsguys.com>
2018-05-15 06:31:14 +0100

    Added missing gitpython dependency to setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     keywords = ['terraform', 'assert', 'testing'],
     packages = find_packages(),
     install_requires=[
-        "pyhcl"
+        "pyhcl",
+        "gitpython"
     ],
 )
